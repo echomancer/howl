e117cdc927160be8a247f636ff48e8661cc16cf6
Jason Perkins <jasontperkins@gmail.com>
2013-08-19 14:09:44 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howl::Application.config.secret_key_base = '83c1fa7f2707f2374bb8e473d6becf6cba4cbd133d4be220d7d5274067263256ceb415b8f393717df2a39472362a7c87c460c3b6e77a86648371e6be18f93986'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Howl::Application.config.session_store :cookie_store, key: '_howl_session'
